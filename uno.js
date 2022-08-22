52693c7533691ee0db8ce39b6a0f988c0ace4d32
Paulina Gonzalez <paulinagonzal@hotmail.com>
2022-08-22 01:14:59 -0400

    segundo commit

--- uno.js
+++ uno.js
@@ -1,7 +1,8 @@
 let tres=3;
 let otroTres='3';
 // aca van varios cambios
-if (tres==otroTres){
+// aqui detecta si es el mismo tipo de dato
+if (tres===otroTres){
     console.log('iguales');
 }
 else{
